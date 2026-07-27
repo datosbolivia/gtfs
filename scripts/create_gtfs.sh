caf0b1edebc53403deeded8088b3c90a4738d9f3
Sylvain Lesage <severo@rednegra.net>
2026-07-27 15:57:18 -0400

    Add README.txt

--- scripts/create_gtfs.sh
+++ scripts/create_gtfs.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-# for now, only create a fake hierarchy
 mkdir -p dist
+echo "Community GTFS for Bolivia
+
+This website contains GTFS feeds for Bolivia.
+
+See https://github.com/datosbolivia/gtfs for more information." > dist/README.txt
+
+# for now, only create a fake hierarchy
 mkdir -p dist/miteleferico
-echo "test" > dist/miteleferico/test.txt
